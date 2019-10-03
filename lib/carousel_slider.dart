dbd658f056ca8e4a49133c33f9d22db50b0867f9
daniel sarmiento cordero <danielsarcor@gmail.com>
2019-10-03 08:06:46 -0500

    Delete pauseAutoPlayOnTouch and add pauseAutoPlayWhileTouching

--- lib/carousel_slider.dart
+++ lib/carousel_slider.dart
@@ -19,15 +19,17 @@ class CarouselSlider extends StatefulWidget {
       this.autoPlayInterval: const Duration(seconds: 4),
       this.autoPlayAnimationDuration = const Duration(milliseconds: 800),
       this.autoPlayCurve: Curves.fastOutSlowIn,
-      this.pauseAutoPlayOnTouch,
+      this.pauseAutoPlayWhileTouching,
       this.enlargeCenterPage = false,
       this.onPageChanged,
-        this.scrollPhysics,
+      this.scrollPhysics,
       this.scrollDirection: Axis.horizontal})
-      : this.realPage = enableInfiniteScroll ? realPage + initialPage : initialPage,
+      : this.realPage =
+            enableInfiniteScroll ? realPage + initialPage : initialPage,
         this.pageController = PageController(
           viewportFraction: viewportFraction,
-          initialPage: enableInfiniteScroll ? realPage + initialPage : initialPage,
+          initialPage:
+              enableInfiniteScroll ? realPage + initialPage : initialPage,
         );
 
   /// The widgets to be shown in the carousel.
@@ -90,11 +92,8 @@ class CarouselSlider extends StatefulWidget {
   /// Defaults to [Curves.fastOutSlowIn].
   final Curve autoPlayCurve;
 
-  /// Sets a timer on touch detected that pause the auto play with
-  /// the given [Duration].
-  ///
-  /// Touch Detection is only active if [autoPlay] is true.
-  final Duration pauseAutoPlayOnTouch;
+  /// Pauses the auto play while the user is touching the carousel area
+  final bool pauseAutoPlayWhileTouching;
 
   /// Determines if current page should be larger then the side images,
   /// creating a feeling of depth in the carousel.
@@ -146,8 +145,10 @@ class CarouselSlider extends StatefulWidget {
   /// Jumps the page position from its current value to the given value,
   /// without animation, and without checking if the new value is in range.
   void jumpToPage(int page) {
-    final index = _getRealIndex(pageController.page.toInt(), realPage, items.length);
-    return pageController.jumpToPage(pageController.page.toInt() + page - index);
+    final index =
+        _getRealIndex(pageController.page.toInt(), realPage, items.length);
+    return pageController
+        .jumpToPage(pageController.page.toInt() + page - index);
   }
 
   /// Animates the controlled [CarouselSlider] from the current page to the given page.
@@ -155,36 +156,109 @@ class CarouselSlider extends StatefulWidget {
   /// The animation lasts for the given duration and follows the given curve.
   /// The returned [Future] resolves when the animation completes.
   Future<void> animateToPage(int page, {Duration duration, Curve curve}) {
-    final index = _getRealIndex(pageController.page.toInt(), realPage, items.length);
-    return pageController.animateToPage(pageController.page.toInt() + page - index,
-        duration: duration, curve: curve);
+    final index =
+        _getRealIndex(pageController.page.toInt(), realPage, items.length);
+    return pageController.animateToPage(
+        pageController.page.toInt() + page - index,
+        duration: duration,
+        curve: curve);
   }
 
   @override
   _CarouselSliderState createState() => _CarouselSliderState();
 }
 
-class _CarouselSliderState extends State<CarouselSlider> with TickerProviderStateMixin {
+class _CarouselSliderState extends State<CarouselSlider>
+    with TickerProviderStateMixin {
   Timer timer;
+  Timer elapsedSecondsTimer;
+  var elapsedSeconds;
 
   @override
   void initState() {
     super.initState();
     timer = getTimer();
+    elapsedSeconds = 0;
+    elapsedSecondsTimer = getElapsedSecondsTimer();
   }
 
   Timer getTimer() {
+    if (widget.autoPlay == false) {
+      return null;
+    }
+
     return Timer.periodic(widget.autoPlayInterval, (_) {
       if (widget.autoPlay) {
-        widget.pageController
-            .nextPage(duration: widget.autoPlayAnimationDuration, curve: widget.autoPlayCurve);
+        widget.pageController.nextPage(
+            duration: widget.autoPlayAnimationDuration,
+            curve: widget.autoPlayCurve);
       }
     });
   }
 
+  Timer getElapsedSecondsTimer() {
+    if ((widget.autoPlay == false) ||
+        (widget.pauseAutoPlayWhileTouching == null)) {
+      return null;
+    }
+
+    return Timer.periodic(Duration(seconds: 1), (_) {
+      var elapsedDuration = Duration(seconds: elapsedSeconds);
+      if (elapsedDuration >= widget.autoPlayInterval) {
+        return setState(() {
+          elapsedSeconds = 0;
+        });
+      }
+
+      setState(() {
+        elapsedSeconds++;
+      });
+    });
+  }
+
   void pauseOnTouch() {
+    if (widget.pauseAutoPlayWhileTouching == true) {
+      timer.cancel();
+      elapsedSecondsTimer.cancel();
+    }
+  }
+
+  void playOnTouchEnd() {
+    if (widget.pauseAutoPlayWhileTouching == true) {
+      resetAutoPlayTimer();
+    }
+  }
+
+  void resetAutoPlayTimer() {
+    Duration elapsedDuration = Duration(seconds: elapsedSeconds);
+    Duration untilNextPage = widget.autoPlayInterval - elapsedDuration;
+
+    Timer(untilNextPage, () {
+      widget.pageController.nextPage(
+          duration: widget.autoPlayAnimationDuration,
+          curve: widget.autoPlayCurve);
+
+      setState(() {
+        elapsedSeconds = 0;
+        elapsedSecondsTimer = getElapsedSecondsTimer();
+        timer = getTimer();
+      });
+    });
+  }
+
+  void resetTimers() {
+    if ((widget.autoPlay == false) ||
+        (timer == null) ||
+        (elapsedSecondsTimer == null)) {
+      return null;
+    }
+
     timer.cancel();
-    timer = Timer(widget.pauseAutoPlayOnTouch, () {
+    elapsedSecondsTimer.cancel();
+
+    setState(() {
+      elapsedSeconds = 0;
+      elapsedSecondsTimer = getElapsedSecondsTimer();
       timer = getTimer();
     });
   }
@@ -192,19 +266,22 @@ class _CarouselSliderState extends State<CarouselSlider> with TickerProviderStat
   Widget getWrapper(Widget child) {
     if (widget.height != null) {
       final Widget wrapper = Container(height: widget.height, child: child);
-      return widget.autoPlay && widget.pauseAutoPlayOnTouch != null
+      return widget.autoPlay && (widget.pauseAutoPlayWhileTouching != null)
           ? addGestureDetection(wrapper)
           : wrapper;
     } else {
-      final Widget wrapper = AspectRatio(aspectRatio: widget.aspectRatio, child: child);
-      return widget.autoPlay && widget.pauseAutoPlayOnTouch != null
+      final Widget wrapper =
+          AspectRatio(aspectRatio: widget.aspectRatio, child: child);
+      return widget.autoPlay && (widget.pauseAutoPlayWhileTouching != null)
           ? addGestureDetection(wrapper)
           : wrapper;
     }
   }
 
-  Widget addGestureDetection(Widget child) =>
-      GestureDetector(onPanDown: (_) => pauseOnTouch(), child: child);
+  Widget addGestureDetection(Widget child) => GestureDetector(
+      onLongPress: () => pauseOnTouch(),
+      onLongPressEnd: (_) => playOnTouchEnd(),
+      child: child);
 
   @override
   void dispose() {
@@ -221,14 +298,16 @@ class _CarouselSliderState extends State<CarouselSlider> with TickerProviderStat
       reverse: widget.reverse,
       itemCount: widget.enableInfiniteScroll ? null : widget.items.length,
       onPageChanged: (int index) {
-        int currentPage = _getRealIndex(index + widget.initialPage, widget.realPage, widget.items.length);
+        int currentPage = _getRealIndex(
+            index + widget.initialPage, widget.realPage, widget.items.length);
         if (widget.onPageChanged != null) {
           widget.onPageChanged(currentPage);
+          resetTimers();
         }
       },
       itemBuilder: (BuildContext context, int i) {
-        final int index =
-            _getRealIndex(i + widget.initialPage, widget.realPage, widget.items.length);
+        final int index = _getRealIndex(
+            i + widget.initialPage, widget.realPage, widget.items.length);
 
         return AnimatedBuilder(
           animation: widget.pageController,
@@ -246,17 +325,22 @@ class _CarouselSliderState extends State<CarouselSlider> with TickerProviderStat
             double value = widget.pageController.page - i;
             value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
 
-            final double height =
-                widget.height ?? MediaQuery.of(context).size.width * (1 / widget.aspectRatio);
-            final double distortionValue =
-                widget.enlargeCenterPage ? Curves.easeOut.transform(value) : 1.0;
+            final double height = widget.height ??
+                MediaQuery.of(context).size.width * (1 / widget.aspectRatio);
+            final double distortionValue = widget.enlargeCenterPage
+                ? Curves.easeOut.transform(value)
+                : 1.0;
 
             if (widget.scrollDirection == Axis.horizontal) {
-              return Center(child: SizedBox(height: distortionValue * height, child: child));
+              return Center(
+                  child:
+                      SizedBox(height: distortionValue * height, child: child));
             } else {
               return Center(
                   child: SizedBox(
-                      width: distortionValue * MediaQuery.of(context).size.width, child: child));
+                      width:
+                          distortionValue * MediaQuery.of(context).size.width,
+                      child: child));
             }
           },
         );
