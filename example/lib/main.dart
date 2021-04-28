309cd001ef3c983d862002047073588354a3fc7b
mahir.ekici <mahir.ekici@albil.com>
2021-04-28 09:38:05 +0300

    bug fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -62,21 +62,21 @@ class CarouselDemoHome extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          DemoItem('Basic demo', '/basic'),
-          DemoItem('No center mode demo', '/nocenter'),
-          DemoItem('Image carousel slider', '/image'),
-          DemoItem('More complicated image slider', '/complicated'),
-          DemoItem('Enlarge strategy demo slider', '/enlarge'),
-          DemoItem('Manually controlled slider', '/manual'),
-          DemoItem('Noon-looping carousel slider', '/noloop'),
-          DemoItem('Vertical carousel slider', '/vertical'),
-          DemoItem('Fullscreen carousel slider', '/paralax'),
-          DemoItem('Carousel with indicator demo', '/indicator'),
-          DemoItem('On-demand carousel slider', '/ondemand'),
-          DemoItem('Image carousel slider with prefetch demo', '/prefetch'),
-          DemoItem('Carousel change reason demo', '/reason'),
-          DemoItem('Keep pageview position demo', '/position'),
-          DemoItem('Multiple item in one screen demo', '/multiple'),
+          // DemoItem('Basic demo', '/basic'),
+          // DemoItem('No center mode demo', '/nocenter'),
+          // DemoItem('Image carousel slider', '/image'),
+          // DemoItem('More complicated image slider', '/complicated'),
+          // DemoItem('Enlarge strategy demo slider', '/enlarge'),
+          // DemoItem('Manually controlled slider', '/manual'),
+          // DemoItem('Noon-looping carousel slider', '/noloop'),
+          // DemoItem('Vertical carousel slider', '/vertical'),
+          DemoItem('ParalaxSliderDemo', '/paralax'),
+          // DemoItem('Carousel with indicator demo', '/indicator'),
+          // DemoItem('On-demand carousel slider', '/ondemand'),
+          // DemoItem('Image carousel slider with prefetch demo', '/prefetch'),
+          // DemoItem('Carousel change reason demo', '/reason'),
+          // DemoItem('Keep pageview position demo', '/position'),
+          // DemoItem('Multiple item in one screen demo', '/multiple'),
         ],
       ),
     );
@@ -333,64 +333,71 @@ class ParalaxSliderDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Fullscreen sliding carousel demo')),
+      appBar: AppBar(title: Text('ParalaxSliderDemo')),
       backgroundColor: Colors.amber,
-      body: Builder(
-        builder: (context) {
-          final double height = 300;
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Builder(
+              builder: (context) {
+                final double height = 300;
 
-          return Container(
-              child: CarouselSlider.builder(
-            itemCount: imgList.length,
-            options: CarouselOptions(
-              height: height,
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              enableInfiniteScroll: false,
-              initialPage: 0,
-              paralaxEffect: true,
+                return Container(
+                    child: CarouselSlider.builder(
+                  itemCount: imgList.length,
+                  options: CarouselOptions(
+                    height: height,
+                    viewportFraction: 1.0,
+                    enlargeCenterPage: false,
+                    enableInfiniteScroll: false,
+                    initialPage: 0,
+                    paralaxEffect: true,
+                    onPageChanged: (index, reason) {
+                      print("index ");
+                    },
+                  ),
+                  itemBuilder: (
+                    context,
+                    index,
+                    realIdx,
+                  ) {
+                    return Stack(
+                      children: [
+                        Container(child: Image.network(imgList[index])),
+                        Text("sdcsdvdvsvsdvsdsd")
+                      ],
+                    );
+                  },
+                ));
+                //MediaQuery.of(context).size.height;
+                // return CarouselSlider(
+                //   options: CarouselOptions(
+                //     height: height,
+                //     viewportFraction: 1.0,
+                //     enlargeCenterPage: false,
+                //     enableInfiniteScroll: false,
+                //     initialPage: 0,
+                //     paralaxEffect: true,
+                //     // autoPlay: false,
+                //   ),
+                //   items: imgList
+                //       .map((item) => Container(
+                //           color: Colors.red,
+                //           child: Image.network(
+                //             item,
+                //             fit: BoxFit.fill,
+                //             height: height,
+                //             width: MediaQuery.of(context).size.width,
+                //           )
+                //           //Center(child: Text("sddcdscdscsd")
+
+                //           ))
+                //       .toList(),
+                // );
+              },
             ),
-            // ignore: missing_return
-
-            itemBuilder: (
-              context,
-              index,
-              realIdx,
-            ) {
-              return Stack(
-                children: [
-                  Container(child: Image.network(imgList[index])),
-                  Text("sdcsdvdvsvsdvsdsd")
-                ],
-              );
-            },
-          ));
-          //MediaQuery.of(context).size.height;
-          // return CarouselSlider(
-          //   options: CarouselOptions(
-          //     height: height,
-          //     viewportFraction: 1.0,
-          //     enlargeCenterPage: false,
-          //     enableInfiniteScroll: false,
-          //     initialPage: 0,
-          //     paralaxEffect: true,
-          //     // autoPlay: false,
-          //   ),
-          //   items: imgList
-          //       .map((item) => Container(
-          //           color: Colors.red,
-          //           child: Image.network(
-          //             item,
-          //             fit: BoxFit.fill,
-          //             height: height,
-          //             width: MediaQuery.of(context).size.width,
-          //           )
-          //           //Center(child: Text("sddcdscdscsd")
-
-          //           ))
-          //       .toList(),
-          // );
-        },
+          ],
+        ),
       ),
     );
   }
