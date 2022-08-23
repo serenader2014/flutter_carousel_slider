ac4edd37155289dfdb5192e628e2a1c4e3ef51cf
Diefferson Santos <diefferson.sts@gmail.com>
2022-08-23 12:21:49 -0300

    Fixing problem with timer after dispose widget

--- lib/carousel_slider.dart
+++ lib/carousel_slider.dart
@@ -125,6 +125,11 @@ class CarouselSliderState extends State<CarouselSlider>
   Timer? getTimer() {
     return widget.options.autoPlay
         ? Timer.periodic(widget.options.autoPlayInterval, (_) {
+            // avoid problems to access context after state dispose
+            if (!mounted) {
+              return;
+            }
+
             final route = ModalRoute.of(context);
             if (route?.isCurrent == false) {
               return;
